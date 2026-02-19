ce931e94aaa194e22ffc3025396b969cd00e1b69
Gabriel L <103595204+GabrAngelis@users.noreply.github.com>
2026-02-19 12:14:09 -0300

    Aula-19-02

---
+++ JS-Conceitos/qualquer.js
@@ -0,0 +1,81 @@
+# Aula 19-02
+
+// let lista = ['JavaScript', 'TypeScript']
+
+// lista.forEach(elemento =>{ 
+//     console.log(elemento)
+// })
+
+// for (let i = 0; i < lista.length; i++) {
+//     console.log(lista[i])
+// }
+
+// let empresa = {
+//     nome: "Fatec",
+//     CNPJ: "23283283"
+// }
+
+// for (atributo in empresa){
+//         console.log(empresa[atributo])
+//     }
+
+// let obj = {
+//     x: 10
+// }
+
+// let obj2 = obj
+// obj2.x = obj2.x + 10
+
+// console.log(obj.x)
+// console.log(obj2.x)
+
+// let obj = {x: 10}
+
+// let fn = (variavel) =>{
+//      variavel.x = variavel.x + 1000
+// }
+
+// fn(obj)
+// console.log(obj.x)
+
+// const empresa = {
+//     nome: "Mercado online",
+//     razaoSocial: "ABC LTDA",
+//     telefone: {
+//         ddd: '00',
+//         numero: '99999999'
+//     }
+// }
+
+// console.log("Telefone da empresa: "+empresa.telefone.ddd+ " "+empresa.telefone.numero)
+
+// function Empresa(nome, cnpj){
+//     this.nome = nome
+//     this.cnpj = cnpj
+// }
+
+// let emp1 = new Empresa('ABC', '123')
+// let emp2 = new Empresa('DFG', '900')
+
+// console.log(emp1)
+// console.log(emp2)
+
+// let empresa = {
+//     nome: 'Fatec',
+//     get pegarNome(){
+//         return this.nome.toUpperCase()
+//     },
+//     set inserirNome(nome){
+//         if(nome != ''){
+//             this.nome = nome
+//         }
+//     }
+// }
+
+// empresa.inserirNome = 'Qualquer'
+
+// console.log(empresa.pegarNome)
+
+// if(1 === '1'){
+//     console.log('iguais')
+// }
